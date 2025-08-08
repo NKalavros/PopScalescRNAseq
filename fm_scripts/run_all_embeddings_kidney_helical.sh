ab4267f7c185087860cddcd9ca0a1f67ac6a0e39
UshiromiayDev <kalavrosnikolaos@gmail.com>
2025-08-08 03:35:16 -0400

    minor syntax changes

---
+++ fm_scripts/run_all_embeddings_kidney_helical.sh
@@ -0,0 +1,176 @@
+# NOTE: This code is slightly outdates because I moved the lake files to two actual directories (lake_scrna and lake_snrna)
+conda activate /gpfs/scratch/nk4167/miniconda/envs/scgpt_env
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_scrna
+mkdir -p embeddings
+if [ ! -f "embeddings/scgpt.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scgpt_embeddings.py  --input lake_scrna_15_genenames.h5ad --output embeddings/scgpt.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_snrna
+mkdir -p embeddings
+if [ ! -f "embeddings/scgpt.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scgpt_embeddings.py  --input lake_snrna_16_genenames.h5ad --output embeddings/scgpt.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Abedini
+mkdir -p embeddings
+if [ ! -f "embeddings/scgpt.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scgpt_embeddings.py  --input GSE211785_Susztak_SC_SN_ATAC_merged_PostSCVI_final_counts.h5ad --output embeddings/scgpt.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/SCP1288
+mkdir -p embeddings
+if [ ! -f "embeddings/scgpt.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scgpt_embeddings.py  --input data.h5ad --output embeddings/scgpt.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Krishna
+mkdir -p embeddings
+if [ ! -f "embeddings/scgpt.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scgpt_embeddings.py  --input data.h5ad --output embeddings/scgpt.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Braun
+mkdir -p embeddings
+if [ ! -f "embeddings/scgpt.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scgpt_embeddings.py  --input data.h5ad --output embeddings/scgpt.h5ad
+fi
+
+# Now let's do some more models - Geneformer
+conda activate /gpfs/scratch/nk4167/miniconda/envs/helical-package
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_scrna
+mkdir -p embeddings
+if [ ! -f "embeddings/geneformer_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneoformer.py  --input lake_scrna_15_genenames.h5ad --output embeddings/geneformer.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_snrna
+mkdir -p embeddings
+if [ ! -f "embeddings/geneformer_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneoformer.py  --input lake_snrna_16_genenames.h5ad --output embeddings/geneformer.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Abedini
+mkdir -p embeddings
+if [ ! -f "embeddings/geneformer_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneoformer.py  --input GSE211785_Susztak_SC_SN_ATAC_merged_PostSCVI_final_counts.h5ad --output embeddings/geneformer.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/SCP1288
+mkdir -p embeddings
+if [ ! -f "embeddings/geneformer_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneoformer.py  --input data.h5ad --output embeddings/geneformer.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Krishna
+mkdir -p embeddings
+if [ ! -f "embeddings/geneformer_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneoformer.py  --input data.h5ad --output embeddings/geneformer.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Braun
+mkdir -p embeddings
+if [ ! -f "embeddings/geneformer_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneoformer.py  --input data.h5ad --output embeddings/geneformer.h5ad
+fi
+
+
+# Now let's do some more models - scFoundation
+conda activate /gpfs/scratch/nk4167/miniconda/envs/scFoundation_env
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_scrna
+mkdir -p embeddings
+# check if the file output file exists
+if [ ! -f "embeddings/scfoundation.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scfoundation_embeddings.py  --input lake_scrna_15_genenames.h5ad --output embeddings/scfoundation.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_snrna
+mkdir -p embeddings
+if [ ! -f "embeddings/scfoundation.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scfoundation_embeddings.py  --input lake_snrna_16_genenames.h5ad --output embeddings/scfoundation.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Abedini
+mkdir -p embeddings
+if [ ! -f "embeddings/scfoundation.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scfoundation_embeddings.py  --input GSE211785_Susztak_SC_SN_ATAC_merged_PostSCVI_final_counts.h5ad --output embeddings/scfoundation.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/SCP1288
+mkdir -p embeddings
+if [ ! -f "embeddings/scfoundation.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scfoundation_embeddings.py  --input data.h5ad --output embeddings/scfoundation.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Krishna
+mkdir -p embeddings
+if [ ! -f "embeddings/scfoundation.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scfoundation_embeddings.py  --input data.h5ad --output embeddings/scfoundation.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Braun
+mkdir -p embeddings
+if [ ! -f "embeddings/scfoundation.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scfoundation_embeddings.py  --input data.h5ad --output embeddings/scfoundation.h5ad
+fi
+
+# Now let's do some more models - SCIMILARITY
+conda activate /gpfs/scratch/nk4167/miniconda/envs/scimilarity_env
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_scrna
+mkdir -p embeddings
+if [ ! -f "embeddings/scimilarity.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scimilarity_embeddings.py  --input lake_scrna_15_genenames.h5ad --output embeddings/scimilarity.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_snrna
+mkdir -p embeddings
+if [ ! -f "embeddings/scimilarity.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scimilarity_embeddings.py  --input lake_snrna_16_genenames.h5ad --output embeddings/scimilarity.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Abedini
+mkdir -p embeddings
+if [ ! -f "embeddings/scimilarity.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scimilarity_embeddings.py  --input GSE211785_Susztak_SC_SN_ATAC_merged_PostSCVI_final_counts.h5ad --output embeddings/scimilarity.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/SCP1288
+mkdir -p embeddings
+if [ ! -f "embeddings/scimilarity.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scimilarity_embeddings.py  --input data.h5ad --output embeddings/scimilarity.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Krishna
+mkdir -p embeddings
+if [ ! -f "embeddings/scimilarity.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scimilarity_embeddings.py  --input data.h5ad --output embeddings/scimilarity.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Braun
+mkdir -p embeddings
+if [ ! -f "embeddings/scimilarity.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/scimilarity_embeddings.py  --input data.h5ad --output embeddings/scimilarity.h5ad
+fi
+
+
+# Now let's do some more models - UCE
+conda activate /gpfs/scratch/nk4167/miniconda/envs/uce_env
+FULL_ENV_PATH='/gpfs/scratch/nk4167/miniconda/envs/uce_env'
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_scrna
+mkdir -p embeddings
+mkdir -p scrna_embeddings
+if [ ! -f "embeddings/uce.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/uce_embeddings.py  --input $(pwd)/lake_scrna_15_genenames.h5ad --output embeddings/uce.h5ad --batch_size 256
+fi
+mv $FULL_ENV_PATH/UCE/scrna_embeddingslake_scrna_15_genenames_uce_adata.h5ad scrna_embeddings/uce.h5ad
+cd /gpfs/scratch/nk4167/KidneyAtlas/lake_snrna
+mkdir -p embeddings
+mkdir -p snrna_embeddings
+if [ ! -f "embeddings/uce.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/uce_embeddings.py  --input $(pwd)/lake_snrna_16_genenames.h5ad --output embeddings/uce.h5ad --batch_size 128
+    mv $FULL_ENV_PATH/UCE/snrna_embeddingslake_snrna_16_genenames_uce_adata.h5ad snrna_embeddings/uce.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Abedini
+mkdir -p embeddings
+if [ ! -f "embeddings/uce.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/uce_embeddings.py  --input $(pwd)/GSE211785_Susztak_SC_SN_ATAC_merged_PostSCVI_final_counts.h5ad --output embeddings/uce.h5ad
+    mv $FULL_ENV_PATH/UCE/embeddingsGSE211785_Susztak_SC_SN_ATAC_merged_PostSCVI_final_counts_uce_adata.h5ad embeddings/uce.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/SCP1288
+mkdir -p embeddings
+if [ ! -f "embeddings/uce.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/uce_embeddings.py  --input $(pwd)/data.h5ad --output embeddings/uce.h5ad
+    mv $FULL_ENV_PATH/UCE/embeddingsdata_uce_adata.h5ad embeddings/uce.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Krishna
+mkdir -p embeddings
+if [ ! -f "embeddings/uce.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/uce_embeddings.py  --input $(pwd)/data.h5ad --output embeddings/uce.h5ad
+    mv $FULL_ENV_PATH/UCE/embeddingsdata_uce_adata.h5ad embeddings/uce.h5ad
+fi
+cd /gpfs/scratch/nk4167/KidneyAtlas/Braun
+mkdir -p embeddings
+if [ ! -f "embeddings/uce.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/uce_embeddings.py  --input $(pwd)/data.h5ad --output embeddings/uce.h5ad
+    mv $FULL_ENV_PATH/UCE/embeddingsdata_uce_adata.h5ad embeddings/uce.h5ad
+fi
--- fm_scripts/run_helical_geneformer.py
+++ fm_scripts/run_helical_geneformer.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument('--workdir', type=str, default='.', help='Working directory to change to')
     parser.add_argument('--batch_size', type=int, default=20, help='Batch size for Geneformer')
     parser.add_argument('--input_file', type=str, required=True, help='Input h5ad file')
+    parser.add_argument('--output_file', type=str, default='embeddings/geneformer_helical.h5ad', help='Output h5ad file')
     args = parser.parse_args()
 
     print(f"Arguments: {args}")
@@ -17,9 +18,12 @@ def main():
     model_config = GeneformerConfig(batch_size=args.batch_size, device= "cuda" if torch.cuda.is_available() else "cpu")
     geneformer_v2 = Geneformer(model_config)
 
-    ann_data = ad.read_h5ad(args.input_file)
-    dataset = geneformer_v2.process_data(ann_data)
+    adata = ad.read_h5ad(args.input_file)
+    dataset = geneformer_v2.process_data(adata)
     embeddings = geneformer_v2.get_embeddings(dataset)
+    adata.obsm['X_geneformer_helical'] = embeddings
+    
+    adata.write(args.output_file)
     print("Base model embeddings shape:", embeddings.shape)
 
 if __name__ == "__main__":
