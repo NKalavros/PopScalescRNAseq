1d3b1a3771c4bc32173cd48bb246675385898a16
UshiromiayDev <kalavrosnikolaos@gmail.com>
2025-08-11 02:17:32 -0400

    Adding helical UCE

--- fm_scripts/run_all_embeddings_breast_helical.sh
+++ fm_scripts/run_all_embeddings_breast_helical.sh
@@ -13,7 +13,7 @@ fi
 cd /gpfs/scratch/nk4167/BreastAtlas/Pal
 mkdir embeddings
 if [ ! -f "embeddings/geneformer_helical.h5ad" ]; then
-    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneformer.py  --input data.h5ad --output embeddings/geneformer_helical.h5ad
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_geneformer.py  --input data.h5ad --output embeddings/geneformer_helical.h5ad --batch_size 80
 fi
 cd /gpfs/scratch/nk4167/BreastAtlas/Gray
 mkdir embeddings
@@ -60,4 +60,38 @@ cd /gpfs/scratch/nk4167/BreastAtlas/Klughammer_scRNA
 mkdir embeddings
 if [ ! -f "embeddings/transcriptformer.h5ad" ]; then
     bash /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_transcriptformer.sh
+fi
+
+
+# Now let's do some more models - Geneformer (BreastAtlas)
+conda activate /gpfs/scratch/nk4167/miniconda/envs/helical-package
+cd /gpfs/scratch/nk4167/BreastAtlas/Bassez
+mkdir embeddings
+if [ ! -f "embeddings/scgpt_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_scgpt.py  --input data.h5ad --output embeddings/scgpt_helical.h5ad
+fi
+cd /gpfs/scratch/nk4167/BreastAtlas/Wu
+mkdir embeddings
+if [ ! -f "embeddings/scgpt_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_scgpt.py  --input data.h5ad --output embeddings/scgpt_helical.h5ad
+fi
+cd /gpfs/scratch/nk4167/BreastAtlas/Pal
+mkdir embeddings
+if [ ! -f "embeddings/scgpt_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_scgpt.py  --input data.h5ad --output embeddings/scgpt_helical.h5ad
+fi
+cd /gpfs/scratch/nk4167/BreastAtlas/Gray
+mkdir embeddings
+if [ ! -f "embeddings/scgpt_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_scgpt.py  --input data.h5ad --output embeddings/scgpt_helical.h5ad
+fi
+cd /gpfs/scratch/nk4167/BreastAtlas/Klughammer_snRNA
+mkdir embeddings
+if [ ! -f "embeddings/scgpt_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_scgpt.py  --input data_with_ensembl.h5ad --output embeddings/scgpt_helical.h5ad
+fi
+cd /gpfs/scratch/nk4167/BreastAtlas/Klughammer_scRNA
+mkdir embeddings
+if [ ! -f "embeddings/scgpt_helical.h5ad" ]; then
+    python /gpfs/scratch/nk4167/PopScalescRNAseq/fm_scripts/run_helical_scgpt.py  --input data_with_ensembl.h5ad --output embeddings/scgpt_helical.h5ad
 fi
---
+++ fm_scripts/run_helical_uce.py
@@ -0,0 +1,50 @@
+
+import argparse
+from helical.models.uce import UCE, UCEConfig # type: ignore
+import anndata as ad #type: ignore
+import os
+import torch #type: ignore
+
+def main():
+    parser = argparse.ArgumentParser(description="Run Geneformer embedding generation.")
+    parser.add_argument('--workdir', type=str, default='.', help='Working directory to change to')
+    parser.add_argument('--batch_size', type=int, default=20, help='Batch size for Geneformer')
+    parser.add_argument('--input_file', type=str, default='data.h5ad', required=False, help='Input h5ad file')
+    parser.add_argument('--output_file', type=str, default='embeddings/helical_scgpt.h5ad', help='Output h5ad file')
+    args = parser.parse_args()
+
+    print(f"Arguments: {args}")
+    os.chdir(args.workdir)
+    model_config = UCEConfig(batch_size=args.batch_size, device= "cuda" if torch.cuda.is_available() else "cpu")
+    uce = UCE(configurer = model_config)
+
+    adata = ad.read_h5ad(args.input_file)
+    adata.var_names_make_unique()
+    dataset = uce.process_data(adata)
+    embeddings = uce.get_embeddings(dataset)
+    adata.obsm['X_geneformer_uce'] = embeddings
+
+    # Sanitize obs and var columns
+    adata.obs.columns = adata.obs.columns.astype(str)
+    adata.var.columns = adata.var.columns.astype(str)
+    # Convert object dtype columns to string
+    for col in adata.obs.columns:
+        if adata.obs[col].dtype == 'object':
+            adata.obs[col] = adata.obs[col].astype(str)
+    for col in adata.var.columns:
+        if adata.var[col].dtype == 'object':
+            adata.var[col] = adata.var[col].astype(str)
+    # Remove problematic _index column if present
+    if '_index' in adata.obs.columns:
+        adata.obs.drop('_index', axis=1, inplace=True)
+    if '_index' in adata.var.columns:
+        adata.var.drop('_index', axis=1, inplace=True)
+    # Drop adata.var index
+    adata.var.reset_index(drop=True, inplace=True)
+
+    adata.write(args.output_file)
+
+    print("Base model embeddings shape:", embeddings.shape)
+
+if __name__ == "__main__":
+    main()
